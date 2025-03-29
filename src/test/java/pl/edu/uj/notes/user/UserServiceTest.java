8df2df4f1161f6bf219e4138f188be1bdc55d5a4
Krystian Sikora <92646188+krystian-sikora@users.noreply.github.com>
2025-03-29 09:54:05 +0100

    rewrite user service unit to integration test (#13)

--- src/test/java/pl/edu/uj/notes/user/UserServiceTest.java
+++ src/test/java/pl/edu/uj/notes/user/UserServiceTest.java
@@ -1,49 +1,50 @@
 package pl.edu.uj.notes.user;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pl.edu.uj.notes.authentication.SecurityConfig;
 import pl.edu.uj.notes.user.exceptions.UserAlreadyExistsException;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Import(SecurityConfig.class)
 public class UserServiceTest {
 
   private static final String USERNAME = "username";
   private static final String PASSWORD = "password";
   private static final String ENCODED_PASSWORD = "encoded";
 
-  @Mock private UserRepository userRepository;
-  @Mock PasswordEncoder passwordEncoder;
-  @InjectMocks private UserService userService;
+  @Autowired private UserRepository userRepository;
+  @Autowired private UserService userService;
+  @MockitoBean private PasswordEncoder passwordEncoder;
+
+  @AfterEach
+  void tearDown() {
+    userRepository.deleteAll();
+  }
 
   @Nested
-  class createUser {
+  class CreateUser {
 
     @Test
     void whenCreateUser_thenReturnsUserId() {
       // Given
       CreateUserRequest createUserRequest = new CreateUserRequest(USERNAME, PASSWORD);
-      UserEntity mockUser = new UserEntity(USERNAME, PASSWORD);
-
-      when(userRepository.existsByUsername(USERNAME)).thenReturn(false);
-      when(userRepository.save(any(UserEntity.class))).thenReturn(mockUser);
 
       // When
       int id = userService.createUser(createUserRequest);
 
       // Then
-      assertEquals(0, id);
-      verify(userRepository, times(1)).save(any(UserEntity.class));
+      assertTrue(userRepository.existsById(id));
+      assertTrue(userRepository.existsByUsername(createUserRequest.getUsername()));
     }
 
     @Test
@@ -51,28 +52,22 @@ public class UserServiceTest {
       // Given
       CreateUserRequest createUserRequest = new CreateUserRequest(USERNAME, PASSWORD);
 
-      when(userRepository.existsByUsername(USERNAME)).thenReturn(true);
-
       // When & Then
+      assertDoesNotThrow(() -> userService.createUser(createUserRequest));
       assertThrows(
           UserAlreadyExistsException.class, () -> userService.createUser(createUserRequest));
-      verify(userRepository, never()).save(any(UserEntity.class));
     }
 
     @Test
-    void createsUserWithEncodedPassword(@Captor ArgumentCaptor<UserEntity> captor) {
+    void createsUserWithEncodedPassword() {
       var request = new CreateUserRequest(USERNAME, PASSWORD);
 
-      when(userRepository.existsByUsername(any())).thenReturn(false);
-      when(passwordEncoder.encode(any())).thenReturn(ENCODED_PASSWORD);
+      when(passwordEncoder.encode(PASSWORD)).thenReturn(ENCODED_PASSWORD);
 
       userService.createUser(request);
+      UserEntity user = userRepository.getUserEntityByUsername(USERNAME).orElseThrow();
 
-      verify(userRepository).save(captor.capture());
-
-      assertThat(captor.getValue())
-          .extracting(UserEntity::getUsername, UserEntity::getPassword)
-          .containsExactly(USERNAME, ENCODED_PASSWORD);
+      assertEquals(ENCODED_PASSWORD, user.getPassword());
     }
   }
 }
