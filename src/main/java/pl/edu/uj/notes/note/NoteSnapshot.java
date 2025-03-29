b5f72c5c81dfeb2bb1b5ce79bc6bb5b66250b43c
Krystian Sikora <s223097@student.uek.krakow.pl>
2025-03-29 21:46:35 +0100

    add update and create date to notes

--- src/main/java/pl/edu/uj/notes/note/NoteSnapshot.java
+++ src/main/java/pl/edu/uj/notes/note/NoteSnapshot.java
@@ -6,8 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.Instant;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Data
 @Entity
@@ -23,6 +26,10 @@ public class NoteSnapshot {
   private Note noteId;
 
   private String content;
+
+  @CreatedDate private Instant createdAt;
+
+  @LastModifiedDate private Instant updatedAt;
 
   public NoteSnapshot(Note noteId, String content) {
     this.noteId = noteId;
