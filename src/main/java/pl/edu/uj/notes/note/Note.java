6fc3e1d53e3d6121c777f18640859dbb7dd08a2e
krystian-sikora <krystians7183@gmail.com>
2025-04-01 21:36:45 +0200

    fix null create and update date

--- src/main/java/pl/edu/uj/notes/note/Note.java
+++ src/main/java/pl/edu/uj/notes/note/Note.java
@@ -6,10 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Note {
 
   @Id
--- src/main/java/pl/edu/uj/notes/note/NoteSnapshot.java
+++ src/main/java/pl/edu/uj/notes/note/NoteSnapshot.java
@@ -1,20 +1,17 @@
 package pl.edu.uj.notes.note;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import java.time.Instant;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class NoteSnapshot {
 
   @Id
