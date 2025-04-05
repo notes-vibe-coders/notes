66c3f9ee64aeb17c37a150890e574477369c9d38
Krystian Sikora <92646188+krystian-sikora@users.noreply.github.com>
2025-04-05 14:58:23 +0200

    fix null create and update date (#39)

---
+++ src/main/java/pl/edu/uj/notes/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package pl.edu.uj.notes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
