9bc62c04be898d67e202adcba0a7b2c1d036bafa
WBZdeb <wojciech.zdeb98@gmail.com>
2025-04-05 14:19:21 +0200

    Note deletion endpoint (#42)
    Created note deletion endpoint

--- src/main/java/pl/edu/uj/notes/advice/GlobalExceptionHandler.java
+++ src/main/java/pl/edu/uj/notes/advice/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.edu.uj.notes.note.exceptions.NoteNotFoundException;
 import pl.edu.uj.notes.user.exceptions.UserNotFoundException;
 
 @RestControllerAdvice
@@ -16,6 +17,11 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  }
+
+  @ExceptionHandler(NoteNotFoundException.class)
+  public ResponseEntity<String> handleNoteNotFoundException(NoteNotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
   }
 }
--- src/test/java/pl/edu/uj/notes/note/NoteControllerTest.java
+++ src/test/java/pl/edu/uj/notes/note/NoteControllerTest.java
@@ -1,9 +1,11 @@
 package pl.edu.uj.notes.note;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -12,6 +14,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.edu.uj.notes.authentication.SecurityConfig;
+import pl.edu.uj.notes.note.exceptions.NoteNotFoundException;
 import pl.edu.uj.notes.user.UserService;
 
 @WebMvcTest(NoteController.class)
@@ -70,5 +73,60 @@ class NoteControllerTest {
     mockMvc
         .perform(post(NOTE_URI).contentType(MediaType.APPLICATION_JSON).content(request))
         .andExpect(status().isCreated());
+  }
+
+  @Test
+  @WithMockUser
+  void deleteNote_blankId_badRequest() throws Exception {
+    var request =
+        """
+            {
+              "id": null
+            }
+            """;
+
+    mockMvc
+        .perform(delete(NOTE_URI).contentType(MediaType.APPLICATION_JSON).content(request))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @WithMockUser
+  void deleteNote_noteNotFound_returnsNotFound() throws Exception {
+    String id = "non-existing-id";
+    DeleteNoteRequest deleteNoteRequest = new DeleteNoteRequest(id);
+    var request =
+        """
+            {
+              "id": "non-existing-id"
+            }
+            """;
+
+    Mockito.doThrow(new NoteNotFoundException("Note with ID " + id + " does not exist"))
+        .when(noteService)
+        .deleteNote(deleteNoteRequest);
+
+    mockMvc
+        .perform(delete(NOTE_URI).contentType(MediaType.APPLICATION_JSON).content(request))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @WithMockUser
+  void deleteNote_noteExists_noContent() throws Exception {
+    String id = "existing-id";
+
+    var request =
+        """
+            {
+              "id": "existing-id"
+            }
+            """;
+
+    Mockito.doNothing().when(noteService).deleteNote(new DeleteNoteRequest(id));
+
+    mockMvc
+        .perform(delete(NOTE_URI).contentType(MediaType.APPLICATION_JSON).content(request))
+        .andExpect(status().isNoContent());
   }
 }
