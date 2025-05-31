0e54da5972f3d862c14c7183b364731629a2cf7d
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-05-31 13:40:06 +0200

    [34] Change post to patch

--- src/main/java/pl/edu/uj/notes/note/SnapshotController.java
+++ src/main/java/pl/edu/uj/notes/note/SnapshotController.java
@@ -3,8 +3,8 @@ package pl.edu.uj.notes.note;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ class SnapshotController {
     return snapshotService.getSnapshotsByNoteId(noteId);
   }
 
-  @PostMapping("/{snapshotId}")
+  @PatchMapping("/{snapshotId}")
   SnapshotDTO restoreSnapshot(
       @PathVariable("noteId") String noteId, @PathVariable("snapshotId") String snapshotId) {
     return snapshotService.restoreSnapshot(noteId, snapshotId);
