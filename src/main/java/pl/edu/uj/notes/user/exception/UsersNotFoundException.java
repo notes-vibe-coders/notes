04ed86b0d5891caa491d346190f1de26d742c868
hubertp21 <hubciop27@gmail.com>
2025-06-14 14:06:54 +0200

    404

--- src/main/java/pl/edu/uj/notes/user/exception/UsersNotFoundException.java
+++ src/main/java/pl/edu/uj/notes/user/exception/UsersNotFoundException.java
@@ -1,5 +1,9 @@
 package pl.edu.uj.notes.user.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UsersNotFoundException extends IllegalArgumentException {
   public UsersNotFoundException(String message) {
     super(message);
